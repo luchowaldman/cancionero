bb5431713f0ed71e55bdfe2e8274d9addc73cdf9
Luis Waldman <luiswaldman@gmail.com>
2025-01-13 22:25:51 -0300

    agrega letra

--- construir_datos/analisisletra.py
+++ construir_datos/analisisletra.py
@@ -75,9 +75,27 @@ def probar(prof, acordes):
         return aco, secu, True
     return aco, secu, False
     
+def subtexto(texto, desde, hasta):
+        if desde < 0:
+            desde = 0
+        if hasta > len(texto):
+            hasta = len(texto)
+        if desde >= hasta:
+            return ""
+        return texto[desde:hasta]
 
-
-
+def letra_separar(verso, acordes):
+    intro = ""
+    partes = []
+    desde = 0
+    for aco in acordes:
+        sb = subtexto(verso, desde, aco['pos'])
+        partes.append(sb)
+        desde = aco['pos']
+    partes.append(subtexto(verso, desde, len(verso)))
+    intro = partes[0]
+    partes.pop(0)
+    return intro, partes
 
 def analizarxletra(banda, tema):
     #print(f"analizarxletra {banda}, {tema}")
@@ -166,12 +184,14 @@ def analizarxletra(banda, tema):
 
        acordes_originales_renglones = []
 
-
+    letras_fin = []
     for renglon in renglones:
-            id_renglon = id_renglon + 1
-            secuenciasagregadas = []
+        id_renglon = id_renglon + 1
+        secuenciasagregadas = []
+        acordesrenglon = []
+        acordes_agregados = []
             #print (renglon)
-            if (renglon['tipo'] == 'm'):
+        if (renglon['tipo'] == 'm'):
                 for acor in renglon['acordes']:
                     if termino_secu:
                         termino_secu = False
@@ -186,6 +206,9 @@ def analizarxletra(banda, tema):
                         acordes_agregados.append(acor['acorde'])
                         termino_secu = len(acordes_de_partes[secu[procesando_secu]])  == len(acordes_agregados)
                         
+                        if (renglon['conl']):
+                            acordesrenglon.append(acor)
+                
                         if termino_secu:
                                 secu_fin.append(secu[procesando_secu])
                                 secuenciasagregadas.append(secu[procesando_secu])
@@ -193,12 +216,26 @@ def analizarxletra(banda, tema):
                                 acordes_agregados.clear()
                     #print (procesando_secu, indice_secu,termino_secu )
 
-
+                letra = ""
                 if (renglon['conl']):
-                    acordes_originales_renglones.append(secuenciasagregadas)
-                    #print("Agrega MUSICA de letra", acordes_originales_renglones)
+                    #print("<-- CPM RENGLON -->", acordesrenglon)
+                    letra = renglon['letra']['letra']
+                    acordes_originales_renglones.append([secuenciasagregadas, acordesrenglon])
+                ini_letra, reng_letra = letra_separar(letra, acordesrenglon)
+                if (ini_letra.strip != '')  and (len(letras_fin) > 0):
+                    try:
+                        texto_paragregar = letras_fin[-1][-1]
+                        letras_fin[-1][-1] = texto_paragregar  + '/n' + ini_letra
+                    except Exception as e:
+                        sory_man = 1
 
-            else:
+                letras_fin.append(reng_letra)
+                if len(letras_fin) == 0:
+                    print("INICIOOO", ini_letra)
+                    letras_fin[0][0] = ini_letra + letras_fin[0][0]
+                
+
+        else:
                 if renglon['_m'] == 'SI':
                     a = 3
 
@@ -219,15 +256,40 @@ def analizarxletra(banda, tema):
 
                     indice_reng  = 0
                     
+                    secuencia_aca, acordes_enrenglon = acordes_originales_renglones[indice_reng]
                     for i in range(texto_desde, ind_texto):
-                        for secu_aca in acordes_originales_renglones[indice_reng]:
+                        for secu_aca in secuencia_aca:
                             secu_fin.append(secu_aca)
                         indice_reng = (indice_reng + 1) % len(acordes_originales_renglones)
-                        #print("Ponemos musica para", renglones[i], secu_fin)
+                        #print("Ponemos musica para", renglones[i]['letra'], secu_aca, acordes_enrenglon)
+                        ini_letra, reng_letra = letra_separar(renglones[i]['letra'], acordes_enrenglon)
+                        if (ini_letra.strip != '')  and (len(letras_fin) > 0):
+                            texto_paragregar = letras_fin[-1][-1]
+                            letras_fin[-1][-1] = texto_paragregar  + '/n' + ini_letra
+                        letras_fin.append(reng_letra)
                         
 
 
+    print("Cancion Completa", acordes_de_partes, secu_fin, letras_fin)
 
+    partes_obj = []
+    
+    i = 0
+    for parte in  acordes_de_partes:
+        i = i + 1
+        parte = Parte(f"parte{i}", parte)
+        partes_obj.append(parte)
+    acordes = Acordes(partes_obj, secu_fin)
+    data = {
+        'tono': 'X',
+        'tempo': 60,        
+        'compas_cantidad': 4,
+        'compas_unidad': 4,
+        'acordes': acordes.toJson(),
+        'orden_partes': secu_fin,
+        'letras': letras_fin,
+    }
+    guardar_tema(banda, tema, data)
 
     #print("Se puede generar completa" , tema)
     return { 'generado': 1 }
---
+++ construir_datos/analizarxletratodos.py
@@ -0,0 +1,64 @@
+from analisisletra  import analizarxletra
+import os
+import json
+
+# Define the directory where the pages will be saved
+SAVE_DIRECTORY = 'cifraclub_pages'
+DIRECTORIO_DATOS = '../cliente/public/data/'
+DIRECTORIO_DATOS_GENERADA = 'data_generada/'
+
+
+
+
+def get_JSON(directorio, archivo):
+    try:
+        with open(f'{directorio}{archivo}.json', 'r') as f:
+            data = json.load(f)
+            return data
+    except Exception as e:
+        #print(f"Error al obtener JSON {directorio}, tema {archivo}: {e}")
+        return None
+    
+
+def obtener_archivos_json(directorio):
+    archivos = []
+    try:
+        for archivo in os.listdir(directorio):
+            if archivo.endswith('.json'):
+                archivos.append(archivo.replace('.json', '')) 
+        return archivos
+    except Exception as e:
+        print(f"Error al leer el directorio: {e}")
+        return []
+
+
+        
+resultados = []
+banda = 'viejas-locas'
+
+
+arch = obtener_archivos_json(DIRECTORIO_DATOS_GENERADA)
+for ar in arch:
+    sp = ar.split('_')
+    if (len(sp) == 2):
+        try:
+            resultados.append(analizarxletra(sp[0], sp[1]))
+        except Exception as e:
+            #print(f"Error al archivo {ar}: {e}")
+            resultados.append({ 'generado': 0 })
+generados = 0
+for gen in resultados:
+    generados = generados + gen['generado']
+
+print("total", len(resultados), "generado", generados)
+
+#analizarxletra('andres calamaro', 'la parte de adelante')
+#analizarxletra('intoxicados', 'esta saliendo el sol')
+#analizarxletra('intoxicados', 'se fue al cielo')
+#analizarxletra('intoxicados', 'casi sin pensar')
+#analizarxletra('intoxicados', 'pila pila')
+#analizarxletra('intoxicados', 'volver a casa')
+#analizarxletra('andres calamaro', 'la parte de adelante')
+#analizarxletra('andres calamaro', 'cuando no estas')
+
+exit()
