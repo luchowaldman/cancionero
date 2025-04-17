9838412bb009e4e20434e2f804f2deee9178f121
Luis Waldman <luiswaldman@gmail.com>
2025-04-16 22:13:53 -0300

    paso algunas referencias a la clase aplicacion

--- cliente/src/App.vue
+++ cliente/src/App.vue
@@ -10,21 +10,14 @@ import Listas from './pages/listas.vue';
 import Editar from './pages/editar.vue';
 import Buscar from './pages/buscar.vue';
 import Configuracion from './pages/configuracion.vue';
+
 import { Reproductor } from './modelo/reproductor';
 
-import { ModeloConfiguracion  } from './modelo/modeloconfiguracion';
 import { Cancion } from './modelo/cancion';
-import { Letra } from './modelo/letra';
 import { Aplicacion } from './modelo/aplicacion';
-
-import { Acordes, Parte } from './modelo/acordes';
 import { EstadoSesion } from './modelo/estadosesion';
 import { Director } from './modelo/director';
-import { item_lista } from './modelo/item_lista';
-import { GetCanciones } from './modelo/GetCanciones';
 import { DirectorOffline } from './modelo/directoroffline';
-import { DirectorOnline } from './modelo/directoronline';
-
 
 
 const aplicacion: Aplicacion = new Aplicacion();
@@ -33,12 +26,8 @@ const aplicacion: Aplicacion = new Aplicacion();
 
 
 // VISTA
-const cancion_ref  = ref(new Cancion("Cancion no cargada", "sin banda", new Acordes([], []), new Letra([])));
-const sesion_ref = ref(new EstadoSesion());
-const compas_ref = ref(-2);
-const editando_item = ref(new item_lista("no song name", "no band name"));
-const editando_cancion = ref(new Cancion("no song name", "no band name", new Acordes([new Parte("p1", ["C"])], [0]), new Letra([[""]]), 120, 4, 4, 4, "C"));
 
+const sesion_ref = ref(new EstadoSesion());
 const bpm_encompas = ref(0);
 
 let reproductor = new Reproductor(2200);
@@ -48,18 +37,16 @@ function startReproduccion()
 {
 
   console.log("Iniciando reproduccion");
-   const seg = 60 / cancion_ref.value.bpm;
+   const seg = 60 / aplicacion.cancion.value.bpm;
    console.log("Duracion:", seg);
    reproductor.setDuracion(seg * 1000);
    reproductor.iniciar();
 }
 
 
-
-
 reproductor.setIniciaCicloHandler(() => {
   faltan_parainicio.value = faltan_parainicio.value - 1;
-  bpm_encompas.value = (bpm_encompas.value + 1) % cancion_ref.value.compas_cantidad;
+  bpm_encompas.value = (bpm_encompas.value + 1) % aplicacion.cancion.value.compas_cantidad;
 });
 
 
@@ -78,7 +65,7 @@ let director: Director = new DirectorOffline(aplicacion.configuracionObj);
     
     sesion_ref.value = directornuevo.configuracion.sesion;
     if (estado_ref.value == 'pausado' && directornuevo.estado != 'pausado') {
-      faltan_parainicio.value = cancion_ref.value.compas_cantidad; 
+      faltan_parainicio.value = aplicacion.cancion.value.compas_cantidad; 
       startReproduccion();
       
     }
@@ -94,48 +81,17 @@ const estado_ref = ref(director.estado);
 const conectado = localStorage.getItem("conectado") || "no";
 
 
-function Conectar() {
-
-  director = new DirectorOnline(aplicacion.configuracionObj);
-  director.setcambiosHandler((directornuevo: Director) => {
-    
-    console.log("Estado _red")
-    sesion_ref.value = directornuevo.configuracion.sesion;
-    estado_ref.value = directornuevo.estado;
-    ctrlMenu.value?.actualizar_vista();
-  });
-
-  director.Iniciar();if (director instanceof DirectorOnline) {
-  // Haz un casting al tipo DirectorOnline y llama al método exclusivo
-  (director as DirectorOnline).Conectar();
-} else {
-  console.log("El objeto no es una instancia de DirectorOnline");
-  }
-  vincular_director();
-}
-function Desconectar() {
-  if (director instanceof DirectorOnline) {
-  // Haz un casting al tipo DirectorOnline y llama al método exclusivo
-  (director as DirectorOnline).Desconectar();
-} else {
-  console.log("El objeto no es una instancia de DirectorOnline");
-  }
-  director = new DirectorOffline(aplicacion.configuracionObj);
-  director.Iniciar();
-  vincular_director();
-}
-
 
 function vincular_director() {
   director_ref.value = director;
   sesion_ref.value = director.configuracion.sesion;
 
   director.setcambiosCancionHandler((cancion: Cancion) => {
-      cancion_ref.value = cancion;
+    aplicacion.cancion.value = cancion;
   });
 
   director.setcambiosCompasHandler((compas: number) => {
-    compas_ref.value = parseInt(compas.toString());
+    aplicacion.compas.value = parseInt(compas.toString());
     });
   }
 
@@ -148,85 +104,12 @@ onMounted(() => {
     aplicacion.Iniciar();
 });
 
-function cargar_edit() {
-  let item = JSON.parse(localStorage.getItem("editando_cancion") || "{}");
-  GetCanciones.obtenerCancion(item).then((cancion_get: Cancion) => {
-      editando_cancion.value = cancion_get;
-    });
-}
-
 
 
 function acciono(valor: string, compas: number = 0) {
-
-  switch (valor) {
-    case 'next':
-      director.click_siguiente();
-      break;
-    case 'previous':
-      director.click_anterior();
-      break;
-      
-    case 'setcancion':
-      director.set_nro_cancion(compas);
-      break;
-    case 'play':
-      director.click_play();
-      break;
-    case 'pause':
-      director.click_pause();
-      break;
-    case 'update-compas':
-      director.update_compas(compas);
-      break;
-    case 'conectar':
-      console.log("conectar");
-      Conectar();
-      break;
-    case 'desconectar':
-      console.log("conectar");
-      Desconectar();
-      break;
-      
-    case 'tocar_cancion':
-      director.set_nro_cancion(compas);
-      break;
-    case 'tocar':
-    case 'listas':
-    case 'config':
-    case 'editar':
-    case 'buscar':
-
-      if (valor == 'editar') 
-      {
-        if (aplicacion.viendo.value == 'tocar') 
-        {
-          editando_item.value = director.getitemActual();
-          localStorage.setItem("editando_cancion", JSON.stringify(editando_item.value));
-          
-        }
-        
-        cargar_edit();
-      }
-      
-      if (valor == 'tocar') 
-      {
-        director.CargarLista();
-
-      }
-
-      aplicacion.viendo.value  = valor;
-      localStorage.setItem("viendo", valor);
-      
-      break;
-    default:
-      console.warn(`Acción no reconocida: ${valor}`);
-  }
-  
+  aplicacion.acciono(valor, compas);
 }
-if (aplicacion.viendo.value  == 'editar') {
-  cargar_edit();
-}
+
 
 
 </script>
@@ -239,24 +122,21 @@ if (aplicacion.viendo.value  == 'editar') {
 <div id="contenedor-musical" class="pantalla">
 
   <Menu 
-  :viendo_vista="aplicacion.viendo.value" :nro_cancion="director_ref.nro_cancion" :sesion="sesion_ref" 
+  :viendo_vista="aplicacion.viendo_pagina.value" :nro_cancion="director_ref.nro_cancion" :sesion="sesion_ref" 
   :total_canciones="director_ref.total_canciones" @acciono="acciono" 
-  :compas="compas_ref" :cancion="cancion_ref" :ref="ctrlMenu"
-  :editando_cancion="editando_cancion" :estado="estado_ref" :conectado="conectado" :director="director_ref"
+  :compas="aplicacion.compas.value" :cancion="aplicacion.cancion.value" :ref="ctrlMenu"
+  :editando_cancion="aplicacion.editando_cancion.value" :estado="estado_ref" :conectado="conectado" :director="director_ref"
    :bpm_encompas="bpm_encompas"
   ></Menu>
-
-
-
   <div class="carteliniciando" v-if="estado_ref=='iniciando'">
         {{ faltan_parainicio }}
    </div>    
 
-    <Tocar v-if="aplicacion.viendo.value =='tocar'"  @acciono="acciono" :compas="compas_ref" :cancion="cancion_ref"></Tocar>
-    <Listas v-if="aplicacion.viendo.value =='listas'" :nro_cancion="director.nro_cancion"  @acciono="acciono"></Listas>
-    <Configuracion v-if="aplicacion.viendo.value =='config'"></Configuracion>
-    <Editar v-if="aplicacion.viendo.value =='editar'"  @acciono="acciono" :cancion="editando_cancion" :item="editando_item"></Editar>
-    <Buscar v-if="aplicacion.viendo.value =='buscar'"  @acciono="acciono"></Buscar>
+    <Tocar v-if="aplicacion.viendo_pagina.value =='tocar'"  @acciono="acciono" :compas="aplicacion.compas.value" :cancion="aplicacion.cancion.value"></Tocar>
+    <Listas v-if="aplicacion.viendo_pagina.value =='listas'" :nro_cancion="director.nro_cancion"  @acciono="acciono"></Listas>
+    <Configuracion v-if="aplicacion.viendo_pagina.value =='config'"></Configuracion>
+    <Editar v-if="aplicacion.viendo_pagina.value =='editar'"  @acciono="acciono" :cancion="aplicacion.editando_cancion.value" :item="aplicacion.editando_item.value"></Editar>
+    <Buscar v-if="aplicacion.viendo_pagina.value =='buscar'"  @acciono="acciono"></Buscar>
 </div>
 </template>
 
--- cliente/src/modelo/aplicacion.ts
+++ cliente/src/modelo/aplicacion.ts
@@ -1,37 +1,132 @@
 import { ref, Ref } from "vue";
 import { EstadoSesion } from "./estadosesion";
 import { ModeloConfiguracion } from "./modeloconfiguracion";
+import { DirectorOffline } from "./directoroffline";
+import { Director } from "./director";
+import { Cancion } from "./cancion";
+import { Acordes } from "./acordes";
+import { Letra } from "./letra";
+import { DirectorOnline } from "./directoronline";
+import { item_lista } from "./item_lista";
+import { GetCanciones } from './GetCanciones';
 
 export class Aplicacion {
     public configuracionObj: ModeloConfiguracion = new ModeloConfiguracion();
-    public viendo: Ref<string> = ref("tocar");
+    public director: Director = new DirectorOffline(this.configuracionObj);
 
+    public viendo_pagina: Ref<string> = ref("tocar");
+    public cancion: Ref<Cancion>  = ref(new Cancion("Cancion no cargada", "sin banda", new Acordes([], []), new Letra([])));
+    public compas: Ref<number> = ref(-2);
+
+    public editando_cancion: Ref<Cancion>  = ref(new Cancion("Cancion no cargada", "sin banda", new Acordes([], []), new Letra([])));
+    public editando_item: Ref<item_lista> = ref(new item_lista("no song name", "no band name"));
     private CargarConfiguracion(): void {
-        this.viendo.value = localStorage.getItem("viendo") || "tocar";        
+        this.viendo_pagina.value = localStorage.getItem("viendo") || "tocar";        
 
-let config_load: string | null = localStorage.getItem("configuracion")
-if (!config_load)
-  config_load = ""
-
-
-
-try {
-  this.configuracionObj = JSON.parse(config_load);
-} catch (error) 
-{
-}
+          
+        let config_load: string | null = localStorage.getItem("configuracion")
+        if (!config_load)
+            config_load = ""
+        try {this.configuracionObj = JSON.parse(config_load);} catch (error) {};
 
 
-this.configuracionObj = new ModeloConfiguracion()
-this.configuracionObj.sesion = new EstadoSesion()
-this.configuracionObj.sesion.nombre = "default"
-this.configuracionObj.nombre = "default"
-localStorage.setItem("configuracion", JSON.stringify(this.configuracionObj))
+        this.configuracionObj = new ModeloConfiguracion()
+        this.configuracionObj.sesion = new EstadoSesion()
+        this.configuracionObj.sesion.nombre = "default"
+        this.configuracionObj.nombre = "default"
+        localStorage.setItem("configuracion", JSON.stringify(this.configuracionObj))
 
     }
 
+    
+
+ cargar_edit() {
+    let item = JSON.parse(localStorage.getItem("editando_cancion") || "{}");
+    console.log("item", item);
+    GetCanciones.obtenerCancion(item).then((cancion_get: Cancion) => {
+        console.log("cancion_get", cancion_get);    
+        this.editando_cancion.value = cancion_get;
+      });
+  }
+  
+
+    public acciono(valor: string, compas: number = 0) {
+
+        switch (valor) {
+          case 'next':
+            this.director.click_siguiente();
+            break;
+          case 'previous':
+            this.director.click_anterior();
+            break;
+            
+          case 'setcancion':
+            this.director.set_nro_cancion(compas);
+            break;
+          case 'play':
+            this.director.click_play();
+            break;
+          case 'pause':
+            this.director.click_pause();
+            break;
+          case 'update-compas':
+            this.director.update_compas(compas);
+            break;
+          case 'conectar':
+            console.log("conectar");
+            this.Conectar();
+            break;
+          case 'desconectar':
+            console.log("conectar");
+            this.Desconectar();
+            break;
+            
+          case 'tocar_cancion':
+            this.director.set_nro_cancion(compas);
+            break;
+          case 'tocar':
+          case 'listas':
+          case 'config':
+          case 'editar':
+          case 'buscar':
+      
+            if (valor == 'editar') 
+            {
+              if (this.viendo_pagina.value == 'tocar') 
+              {
+                this.editando_item.value = this.director.getitemActual();
+                localStorage.setItem("editando_cancion", JSON.stringify(this.editando_item.value));                
+              }              
+              this.cargar_edit();
+            }
+            
+            if (valor == 'tocar') 
+            {
+              this.director.CargarLista();      
+            }      
+            this.viendo_pagina.value  = valor;
+            localStorage.setItem("viendo", valor);            
+            break;
+          default:
+            console.warn(`Acción no reconocida: ${valor}`);
+        }
+        
+      }
+
+      
+      
+    Conectar() {
+    }
+  Desconectar() {
+    
+  }
+  
+
     Iniciar(): void {
         this.CargarConfiguracion();
+        if (this.viendo_pagina.value == 'editar') {
+            this.cargar_edit();
+        }
         console.log("La aplicación ha iniciado.");
 
     }
